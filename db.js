55235f37a745b83bf735dc35a5f27912dc47d67d
Kyle Ketchmark <kyle.r.ketchmark@gmail.com>
2022-01-26 08:07:52 -0800

    change

--- db.js
+++ db.js
@@ -6,3 +6,9 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 const db = new Sequelize("postgres://postgres:dbLocal@localhost:5432/Twournals")
 
 module.exports = db
+
+// const {Sequelize} = require("sequelize")
+
+// const db = new Sequelize("postgres://postgres:dbLocal@localhost:5432/Twournals")
+
+// module.exports = db;
