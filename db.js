5e8fde9189be435a63202989ffba4a1fb4250433
Kyle Ketchmark <kyle.r.ketchmark@gmail.com>
2022-01-26 08:23:55 -0800

    db update

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
+const { Sequelize } = require("sequelize")
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     ssl: process.env.ENVIRONMENT === 'production'
 })
 
-const db = new Sequelize("postgres://postgres:dbLocal@localhost:5432/Twournals")
+module.exports = sequelize
 
-module.exports = db
-
-// const {Sequelize} = require("sequelize")
 
 // const db = new Sequelize("postgres://postgres:dbLocal@localhost:5432/Twournals")
 
