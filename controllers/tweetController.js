95921dc89383dac2a790f822b080b98e8762b6e4
Kyle Ketchmark <kyle.r.ketchmark@gmail.com>
2022-01-25 21:15:53 -0800

    final server changes

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -1,91 +1,85 @@
-// const Express = require('express')
-// const router = require('express').Router()
-// const {models} = require('../models')
+const Express = require('express')
+const router = require('express').Router()
+const { models } = require('../models')
 
-// // router.get('/practice', (req, res) => {
-// //     res.send('Practice Route is working!')
-// // })
 
-// // NEED to be logged in to access Twitter db
-// router.post('/create', async (req, res) => {
-//     const{username, tweetBody, datePublished} = req.body;
-//     const{id} = req.user;
-//     const newId = req.params.id;
-//     const tweetEntry = {
-//         username,
-//         tweetBody,
-//         datePublished,
-//         userId: id,
-//         twournalId: newId
-//     } 
+router.post('/create', async (req, res) => {
+        const { username, tweetBody, datePublished } = req.body;
+        const { id } = req.user;
+        const newId = req.params.id;
+        const tweetEntry = {
+                username,
+                tweetBody,
+                datePublished,
+                userId: id,
+                // twournalId: newId
+        }
+        try {
+                const newTweet = await models.TweetModel.create(tweetEntry);
+                res.status(200).json(newTweet);
+        } catch (err) {
+                res.status(500).json({ error: err });
+        }
+});
+
+// Find all Tweets for logged in user
+router.get('/', async (req, res) => {
+        const { id } = req.user;
+        try {
+                const results = await models.TweetModel.findAll({
+                        where: { userId: id }
+                });
+                res.status(200).json(results);
+        } catch (err) {
+                res.status(500).json({ error: err })
+        }
+});
+
+
+// router.put("/update/:mealId", async (req, res) => {
+//     const {foodName, protein, carbs, fats, kCal, mealCat} = req.body;
+//     const entryId = req.params.mealId;
+//     const userId = req.user.id;
+
+//     const query = {
+//         where: {
+//             id: entryId,
+//             owner: userId
+//         }
+//     };
+//     const updatedMeal = {
+//         foodName: foodName,
+//         protein: protein,
+//         carbs: carbs,
+//         fats: fats,
+//         kCal: kCal,
+//         mealCat: mealCat
+//     };
+
 //     try{
-//         const newTweet = await models.TweetModel.create(tweetEntry);
-//         res.status(200).json(newTweet);
-//     } catch (err) {
+//         const update = await TweetModel.update(updatedMeal, query);
+//         res.status(200).json(update);
+//     } catch(err) {
 //         res.status(500).json({error: err});
 //     }
 // });
 
-// // Find all Tweets for logged in user
-// router.get('/:id', async (req, res) => {
-//     const {id} = req.user;
+router.delete("/delete/:id", async (req, res) => {
+        const {id} = req.user;
+        const tweetId = req.params.id;
 
-//     try {
-//         const results = await models.TweetModel.findAll({
-//             where: {owner: id}
-//         });
-//         res.status(200).json(results);
-//     } catch(err) {
-//         res.status(500).json({error: err})
-//     }
-// });
+        try {
+                const query = {
+                        where: {
+                                userId: id
+                        }
+                };
 
+                await models.TweetModel.destroy(query)
+                res.status(200).json({ message: "Tweet Entry Removed" });
+        } catch (err) {
+                res.status(500).json({ error: err })
+        }
+});
 
-// // router.put("/update/:mealId", async (req, res) => {
-// //     const {foodName, protein, carbs, fats, kCal, mealCat} = req.body;
-// //     const entryId = req.params.mealId;
-// //     const userId = req.user.id;
-
-// //     const query = {
-// //         where: {
-// //             id: entryId,
-// //             owner: userId
-// //         }
-// //     };
-// //     const updatedMeal = {
-// //         foodName: foodName,
-// //         protein: protein,
-// //         carbs: carbs,
-// //         fats: fats,
-// //         kCal: kCal,
-// //         mealCat: mealCat
-// //     };
-
-// //     try{
-// //         const update = await TweetModel.update(updatedMeal, query);
-// //         res.status(200).json(update);
-// //     } catch(err) {
-// //         res.status(500).json({error: err});
-// //     }
-// // });
-
-// router.delete("/delete/:id", async (req, res) => {
-//     const ownerId = req.user.id;
-//     const tweetId = req.params.id;
-
-//     try {
-//         const query = {
-//             where: {
-//                 id: tweetId,
-//                 owner: ownerId
-//             }
-//         };
-
-//         await models.TweetModel.destroy(query)
-//         res.status(200).json({message: "Twournal Entry Removed"});
-//     }   catch(err) {
-        // res.status(500).json({error: err})
-//     }
-// });
-
-// module.exports = router;
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -2,14 +2,18 @@ const db = require('../db')
 
 const UserModel = require('./userModel');
 const TwournalModel = require('./twournalModel');
+const TweetModel = require('./tweetModel')
 
-TwournalModel.belongsTo(UserModel);
 UserModel.hasMany(TwournalModel);
+UserModel.hasMany(TweetModel);
+TwournalModel.belongsTo(UserModel);
+TweetModel.belongsTo(UserModel);
 
 module.exports = {
     dbConnection: db,
     models: {
     UserModel,
-    TwournalModel
+    TwournalModel,
+    TweetModel
     }
 }
